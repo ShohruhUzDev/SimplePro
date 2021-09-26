67176ba7fe3c969ade32a8daf0b6a975125bc48c
ShohruhUzDev <shohruh7464784@mail.ru>
2021-09-26 18:13:04 +0500

    Add DbContext class

---
+++ SimplePro.EntityFramework/SimpleProDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimplePro.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimplePro.EntityFramework
+{
+   public  class SimpleProDbContext:DbContext
+    {
+      public   DbSet<User> Users { get; set; }
+      public  DbSet<Account> Accounts { get; set; }
+        public DbSet<AssetTransaction> AssetTransactions { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AssetTransaction>().OwnsOne(a => a.Stock);
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=SimplePro; Trusted_Connection=true;");
+
+
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
